17d61c1c8e007b4bdf1879bcf37ea8a2d5e8e42b
bosshogg86 <jeffhogg86@gmail.com>
2020-09-03 14:47:39 -0700

    adds questions

--- index.js
+++ index.js
@@ -1,12 +1,38 @@
 const questions = [
-
+  {
+    type: "input",
+    name: "title",
+    message: "What is the name of your project?",
+  },
+  {
+    type: "input",
+    name: "description",
+    message: "Describe your project",
+  },
+  {
+    type: "input",
+    name: "toc",
+    message: "What is your favorite hobby?",
+  },
+  {
+    type: "input",
+    name: "food",
+    message: "What is your favorite food?",
+  },
+  {
+    type: "input",
+    name: "github",
+    message: "Enter your GitHub Username",
+  },
+  {
+    type: "input",
+    name: "linkedin",
+    message: "Enter your LinkedIn URL.",
+  },
 ];
 
-function writeToFile(fileName, data) {
-}
+function writeToFile(fileName, data) {}
 
-function init() {
-
-}
+function init() {}
 
 init();
