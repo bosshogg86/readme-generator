30a2f694f210e22d378b93ca37757210a287e24d
bosshogg86 <jeffhogg86@gmail.com>
2020-09-04 16:45:59 -0700

    async init, try catch

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const inquirer = require("inquirer");
 const fs = require("fs");
 const util = require("util");
-// const api = require("./utils/api.js");
-const generateMarkdown = require("./scripts/generateMarkdown.js");
 const writeFileAsync = util.promisify(fs.writeFile);
+// const generateMarkdown = require("./scripts/generateMarkdown.js");
+// const api = require("./utils/api.js");
 
 const questions = [
   {
@@ -37,25 +37,24 @@ function generateReadMe(answers) {
       ${answers.title}
       ${answers.description}
       ${answers.github}
-      ${answers.email}<
+      ${answers.email}
      `;
 }
 
-promptUser()
-  .then(function (answers) {
+async function init() {
+  try {
+    const answers = await promptUser();
+
     const readMe = generateReadMe(answers);
 
-    return writeFileAsync("README.md", readMe);
-  })
-  .then(function () {
+    await writeFileAsync("README.md", readMe);
+
     console.log("Successfully wrote to README.md");
-  })
-  .catch(function (err) {
+  } catch (err) {
     console.log(err);
-  });
+  }
+}
 
-// function writeToFile(fileName, data) {}
+init();
 
-// function init() {}
-
-// init();
+// module.exports = answers;
--- scripts/generateMarkdown.js
+++ scripts/generateMarkdown.js
@@ -1,8 +1,14 @@
-function generateMarkdown(data) {
-  return `
-# ${data.title}
+const answers = require("../index.js");
 
-`;
+async function generateMarkdown(answers) {
+  console.log(answers);
+  await answers;
+  return `
+      ${answers.title}
+      ${answers.description}
+      ${answers.github}
+      ${answers.email}
+     `;
 }
 
 module.exports = generateMarkdown();
