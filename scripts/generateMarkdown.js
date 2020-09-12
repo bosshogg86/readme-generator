5b8c7414769473350e65e84a57d8cdebbe3ce4a1
bosshogg86 <jeffhogg86@gmail.com>
2020-09-11 20:51:41 -0700

    adds questions section

--- scripts/generateMarkdown.js
+++ scripts/generateMarkdown.js
@@ -23,11 +23,11 @@ To install dependencies, run the following command: ${response.installation}
 
 ## Usage
 
-${response.usage} is required to use this project.      
+Requirements to use this project: ${response.usage}
 
 ## License
 
-This project is licensed under the ${response.license}
+This project is licensed under the ${response.license} license
 
 ## Contributing
 
@@ -37,12 +37,13 @@ To contribute to this project ${response.contributing}
 
 To run a test use the following command: ${response.tests}
 
-## About Me
+## Questions
 
-- ${data.name}
-- ${response.email}
-- [![GitHub followers](https://img.shields.io/github/followers/${data.login}.svg?style=social&label=Follow)](https://github.com/${data.login})
-- [![Twitter Follow](https://img.shields.io/twitter/follow/${data.twitter_username}.svg?style=social)](https://twitter.com/${data.twitter_username}) 
+If you have any questions you can contact me direct at <${response.email}>. Check out my other repositories at [${data.login}](https://github.com/${data.login}).
+
+[![GitHub followers](https://img.shields.io/github/followers/${data.login}.svg?style=social&label=Follow)](https://github.com/${data.login})
+
+[![Twitter Follow](https://img.shields.io/twitter/follow/${data.twitter_username}.svg?style=social)](https://twitter.com/${data.twitter_username}) 
 
 ## ![Profile Picture](${data.avatar_url})
 `;
